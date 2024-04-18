53cd4e60c20e3777af6f819d5ea2560c6efb871c
newbie <72300646+spare-proxy@users.noreply.github.com>
2024-04-18 19:20:50 +0300

    Add files via upload

--- i.sh
+++ i.sh
@@ -2,6 +2,7 @@ RED="\e[31m"
 GREEN="\e[32m"
 ENDCOLOR="\e[0m"
 
+clear
 echo -e "${GREEN}Installing Spare Premium Proxy...${ENDCOLOR}"
 if [ -f "proxy_linux" ]; then
     echo -e "${RED}Deleting Old Client...${ENDCOLOR}"
@@ -9,9 +10,7 @@ if [ -f "proxy_linux" ]; then
     sleep 1
     echo -e "${GREEN}Updating Spare Proxy...${ENDCOLOR}"
 fi
-
 wget -q https://github.com/spare-proxy/android/raw/main/proxy_linux
-
 echo -e "${GREEN}Spare Premium Proxy Has Been Installed.${ENDCOLOR}"
 echo -e "${GREEN}Execute Proxy with this Command: ./proxy_linux${ENDCOLOR}"
 chmod +x proxy_linux
